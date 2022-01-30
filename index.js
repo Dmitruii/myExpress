681f43dcc5080e3c2cb62fe6543644cc88abfbf9
Dima <dimanaymenko2005@gmail.com>
2022-01-30 13:51:27 +0200

    Connect to DB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Application = require('./framework/Application')
 const userRouter = require('./src/user-router')
 const jsonParser = require('./framework/parseJson')
 const parseUrl = require('./framework/parseUrl')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 666
 const app = new Application()
@@ -11,7 +12,18 @@ app.use(parseUrl(`http://localhost:${PORT}`))
 
 app.addRouter(userRouter)
 
-app.listen(PORT, e => console.log(`Server was started correctly on PORT ${PORT}`))
+const DB_URL = 'mongodb+srv://DimaNaym:8S53baN4@cluster0.ouxyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const start = async () => {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log(`Server was started correctly on PORT ${PORT}`))
+    }   catch (e) {
+        console.log(e)
+    }
+}
+
+start()
 
 /**  // Old method how to use express implementation
 *    const server = http.createServer((req, res) => {
